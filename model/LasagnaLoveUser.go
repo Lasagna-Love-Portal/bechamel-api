0b55a86d895f3a584fb6c0b12508022f12518404
mattkuznicki-ll <matt@lasagnalove.org>
2023-05-31 13:20:16 -0500

    Initial support for refresh tokens from /login API:
    * Send back access token and refresh token from the login API.
      Currently validation of these is no different, left separate validation
      in place for future use where refresh tokens should be checked
      against a database for validity.
    * Added ability to pass request_token or a username/password combination
      to the login API.
    * Minor refactoring along the way

--- model/LasagnaLoveUser.go
+++ model/LasagnaLoveUser.go
@@ -1,24 +1,10 @@
 package model
 
-import "encoding/json"
-
 type LasagnaLoveUser struct {
 	ID                 int    `json:"id"`
 	Username           string `json:"username"`
-	Password           string `json:"password"`
+	Password           string `json:"-"`
 	GivenName          string `json:"given_name"`
 	MiddleOrMaidenName string `json:"middle_or_maiden_name"`
 	FamilyName         string `json:"family_name"`
-}
-
-// This overrides the default marshaling of the structure to JSON, removing the password field value.
-// It has the unfortunate side effect of leaving an empty string entry in the generated JSON
-// which leaks out to users, but does allow the use of the same struct for GETting and POSTing user profiles.
-//
-// Better ideas are welcomed.
-func (l LasagnaLoveUser) MarshalJSON() ([]byte, error) {
-	type lasagnaLoveUser LasagnaLoveUser // prevent recursion
-	x := lasagnaLoveUser(l)
-	x.Password = ""
-	return json.Marshal(x)
 }
